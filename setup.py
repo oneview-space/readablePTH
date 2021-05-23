e33b53f54191879825a2af7b7e7d644f026c5bec
ophir <ophir@oneview.space>
2021-05-23 13:17:00 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='readablePTH',
     version='0.1',
     packages=['RPTH'],
-    url='',
+    url="https://github.com/oneview-space/readablePTH",
     license='',
     author='OneView',
     author_email='ophir@oneview.ai',
     description='Make pth readable ',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     entry_points={
         'console_scripts': [
             'rpth = RPTH:main',
