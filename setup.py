557e78d9defd3fac606242792b1fafa68b5a663a
ophir <ophir@oneview.space>
2021-05-23 14:51:49 +0300

    v0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='readablePTH',
-    version='0.1.2',
+    version='0.1.3',
     packages=['RPTH'],
     url="https://github.com/oneview-space/readablePTH",
     license='',
